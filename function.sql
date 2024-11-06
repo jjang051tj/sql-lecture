95d3f9f22f152ae731b160ac02381557a40b28f8
jjang051tj <jjang051tj@gmail.com>
2024-11-06 15:49:22 +0900

    날짜 함수

--- function.sql
+++ function.sql
@@ -120,6 +120,124 @@ FROM dual;
 
 -- xxxxxhello oraclexxxx에서 왼쪽 x만 지워보기, 오른쪽 x지워보기, 좌우 x지우기..
 
+SELECT TRIM('x' FROM 'xxxxxhello oraclexxxx') AS trim,
+	   LTRIM('xxxxxhello oraclexxxx','x') AS ltrim,
+	   RTRIM('xxxxxhello oraclexxxx','x') AS rtrim
+FROM dual;
+
+
+
+-----숫자관련 함수
+--round(값,소수점 자리수) 만약 음수이면 자연수 자릿수
+SELECT 
+	ROUND(1234.5678)   AS round_0,
+	ROUND(1234.4678)   AS not_round_0,
+	ROUND(1234.5678,1) AS round_1,
+	ROUND(1234.5478,1) AS not_round_1,
+	ROUND(1234.5678,2) AS round_2,
+	ROUND(1234.5648,2) AS not_round_2,
+	ROUND(1234.5648,-1) AS round_negative_1,
+	ROUND(1235.5648,-1) AS not_round_negative_1,
+	ROUND(1254.5648,-2) AS round_negative_2,
+	ROUND(1235.5648,-2) AS not_round_negative_2
+FROM dual;
+
+
+--절사
+SELECT 
+	TRUNC(1234.5678)   AS trunc_0,
+	TRUNC(1234.4678)   AS not_trunc_0,
+	TRUNC(1234.5678,1) AS trunc_1,
+	TRUNC(1234.5478,1) AS not_trunc_1,
+	TRUNC(1234.5678,2) AS trunc_2,
+	TRUNC(1234.5648,2) AS not_trunc_2,
+	TRUNC(1234.5648,-1) AS trunc_negative_1,
+	TRUNC(1235.5648,-1) AS not_trunc_negative_1,
+	TRUNC(1254.5648,-2) AS trunc_negative_2,
+	TRUNC(1235.5648,-2) AS not_trunc_negative_2
+FROM dual;
+
+
+SELECT 
+	CEIL(3.14) AS ceil,
+	FLOOR(3.94) AS floor
+FROM dual;
+
+SELECT MOD(10,2) AS mod_1
+FROM dual;
+
+
+--날짜 관련 함수....
+SELECT SYSDATE FROM dual;
+
+--어제
+SELECT 
+	SYSDATE - 1 AS yesterday,
+	SYSDATE + 1 AS tomorrow,
+	SYSDATE + 30 AS after_month
+FROM dual;
+
+SELECT 
+	SYSDATE AS now,
+	ADD_MONTHS(SYSDATE,3) AS after_one_month
+FROM dual;
+
+-- 모든 사원의 10주년 뽑아보기... 
+SELECT * FROM emp; 
+
+SELECT EMPNO,ename,HIREDATE,ADD_MONTHS(HIREDATE,120) AS ten_annuary FROM emp;
+
+
+-- 몇달 차이...
+SELECT empno,ename,hiredate, sysdate ,
+MONTHS_BETWEEN(HIREDATE,SYSDATE) AS month01 ,
+MONTHS_BETWEEN(SYSDATE,hiredate) AS month02 ,
+TRUNC(MONTHS_BETWEEN(SYSDATE,hiredate)) AS trunc_month ,
+TRUNC(MONTHS_BETWEEN(SYSDATE,hiredate)/12) AS trunc_year
+FROM emp;
+
+SELECT SYSDATE,
+	NEXT_DAY(SYSDATE,'월요일'),
+	NEXT_DAY(SYSDATE,'목요일'),
+	LAST_DAY(SYSDATE) 
+FROM dual;
+
+--세기   21c,  2001
+-- cc 세기
+-- YYYY 4자리 연도
+-- Q 분기
+-- DDD 365기분 몇번재 날인지...
+SELECT SYSDATE,
+	ROUND(SYSDATE,'CC') AS format_CC,
+	ROUND(SYSDATE,'YYYY') AS format_YYYY,
+	ROUND(SYSDATE,'Q') AS format_Q,
+	ROUND(SYSDATE,'DDD') AS format_DDD,
+	ROUND(SYSDATE,'HH') AS format_HH
+FROM dual;
+
+SELECT SYSDATE,
+	TRUNC(SYSDATE,'CC') AS format_CC,
+	TRUNC(SYSDATE,'YYYY') AS format_YYYY,
+	TRUNC(SYSDATE,'Q') AS format_Q,
+	TRUNC(SYSDATE,'DDD') AS format_DDD,
+	TRUNC(SYSDATE,'HH') AS format_HH
+FROM dual;
+
+
+--형변환  type casting  자기가 봐서 대강 바꿀만 하다 싶으면 바꿔줌..  명시적
+SELECT empno,ename,empno+'1000' FROM emp;
+
+ALTER SESSION SET NLS_LANGUAGE= 'AMERICAN';
+
+-- TO_CHAR
+SELECT sysdate,
+	TO_CHAR(SYSDATE,'YYYY/MM/DD / HH24 : MI : SS')  AS now,
+	TO_CHAR(SYSDATE,'YYYY/MM/DD / AM HH : MI : SS')  AS now_apm,
+	TO_CHAR(SYSDATE,'DAY, DDTH MON YYYY','NLS_DATE_LANGUAGE=ENGLISH')  AS now02
+FROM dual;
+
+
+
 
 
 
