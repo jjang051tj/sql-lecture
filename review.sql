41e8244769474948815114a1d8e2057d872cdbfb
jjang051tj <jjang051tj@gmail.com>
2024-11-08 12:01:25 +0900

    review

--- review.sql
+++ review.sql
@@ -87,10 +87,68 @@ SELECT empno,ename,sal, deptno FROM emp WHERE DEPTNO = 10
 UNION ALL 
 SELECT empno,ename,sal, deptno FROM emp WHERE DEPTNO = 10;
 
+--함수 문자열 함수 숫자 함수 타입변경
+SELECT LOWER(ename),UPPER(ename),INITCAP(ename) FROM emp;
+SELECT ename,
+	   LENGTH(ename), 
+	   LOWER(SUBSTR(ename,1,2)),
+	   LOWER(SUBSTR(ename,-3,3)),
+	   RPAD('Oralce',10,'*'),
+	   RPAD('120304-',14,'*'),
+	   LPAD('120304',14,'*')
+FROM emp;
+
+--이름의 첫글자와마지막 글자만 보여주고 나머지는 * 채우기
+S***H
+
+SELECT
+	ename , ename AS masked_name
+FROM emp;
 
 
+SELECT ENAME,
+	RPAD(
+		RPAD(SUBSTR(ename,1,1),LENGTH(ename)-1, '*'), --S***
+			 LENGTH(ename),
+			 SUBSTR(ename,LENGTH(ename)
+		)
+	)  AS making_name
+FROM emp;
+SELECT ENAME,SUBSTR(ename,1,1) || 
+			 RPAD('*',LENGTH(ENAME) - 2,'*') || 
+			 SUBSTR(ename,-1,1) 
+	  AS making_name
+FROM emp;
+-- 반올림
+SELECT 
+	ROUND(1234.5678) AS ROUND,
+	ROUND(1234.5678,0) AS ROUND_0,
+	ROUND(1234.5678,1) AS ROUND_1,
+	ROUND(1234.5678,2) AS ROUND_2,
+	ROUND(1234.5678,3) AS ROUND_3,
+	TRUNC(1234.5678) AS TRUNC_0,
+	TRUNC(1234.5678,1) AS TRUNC_1,
+	TRUNC(1234.5678,2) AS TRUNC_2,
+	TRUNC(1234.5678,3) AS TRUNC_3,
+	TRUNC(1234.5678,4) AS TRUNC_4
+FROM dual;
 
+SELECT SYSDATE - 1 AS yesterday FROM dual;
 
+SELECT SYSDATE , 
+	TO_CHAR(SYSDATE,'YYYY/MM/DD HH24:MI:SS') AS year FROM dual;
+
+SELECT nvl(comm,0),nvl2(comm,'O','X') FROM emp;
+
+-- 다중행 함수 여러개의 결과를 하나의 row에 출력하기...
+SELECT 
+	   DEPTNO,
+	   sum(sal) AS sum,
+	   count(*) AS count,
+	   avg(sal) AS sal_avg
+FROM emp
+GROUP BY DEPTNO
+	HAVING avg(sal) >= 2000;
 
 
 
