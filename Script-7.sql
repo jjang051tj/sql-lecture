ae368bba7c5491b7d46629415698969ae589816a
jjang051tj <jjang051tj@gmail.com>
2024-11-12 12:23:12 +0900

    object sequence

---
+++ Script-7.sql
@@ -0,0 +1,2 @@
+GRANT CREATE synonym TO scott;
+GRANT CREATE public synonym TO scott;
--- object.sql
+++ object.sql
@@ -110,5 +110,56 @@ MAXVALUE 99
 CYCLE ;
 -- 1,2,3,4,5,6,7,8 중간에 삭제한다고 끼워 넣기 없다.
 
+DROP SEQUENCE seq_dept;
 
+--테이블 이름에 별명 이건 sys에서 하는 거 
+GRANT CREATE synonym TO scott;
+GRANT CREATE public synonym TO scott;
+
+--테이블의 이름이 길어서 타이핑 하기 힘들때 쓴다.
+CREATE synonym e FOR emp;
+SELECT * FROM e;
+
+--practice01
+--다음 SQL문을 작성해 보세요.          
+--① EMP 테이블과 같은 구조의 데이터를 저장하는 EMPIDX 테이블을 만들어 보세요.
+--② 생성한 EMPIDX 테이블의 EMPNO 열에 IDX_EMPIDX_EMPNO 인덱스를 만들어 보세요.
+--③ 마지막으로 인덱스가 잘 생성되었는지 적절한 데이터 사전 뷰를 통해 확인해 보세요.
+
+--①번
+CREATE TABLE empidx AS
+SELECT * FROM emp;
+
+--②번
+CREATE INDEX IDX_EMPIDX_EMPNO ON empidx(EMPNO);
+
+--③번
+SELECT * FROM user_indexes;
+
+
+--practice02
+--문제 1번에서 생성한 EMPIDX 테이블의 데이터 중 급여 (SAL) 가 1500 초과인 
+--사원들만 출력하 는 EMPIDX_OVER15K 뷰를 생성해 보세요. 
+--이 이름을 가진 뷰가 이미 존재할 경우에 새로운 내용으로 대체 가능해야 합니다. 
+--EMPIDX_OVER15K 뷰는 사원 번호, 사원 이름, 직책, 
+--부서 번호,  급여, 추가 수당 열을 가지고 있습니다. 
+--추가 수당 열의 경우에 추가 수당이 존재하면 O, 존재하지  않으면 X로 출력합니다.
+
+CREATE OR REPLACE VIEW EMPIDX_OVER15K AS 
+(SELECT empno,ename,job,deptno,sal,nvl2(comm,'o','x') AS comm
+FROM EMPIDX
+WHERE sal >= 1500);
+
+SELECT * FROM EMPIDX_OVER15K;
+
+--practice03
+CREATE TABLE deptseq AS SELECT * FROM dept;
+
+CREATE SEQUENCE seq_deptseq
+INCREMENT BY 1
+START WITH 1
+MAXVALUE 99
+MINVALUE 1
+NOCYCLE
+NOCACHE;
 
