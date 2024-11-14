de9c9f17495c3a91f20f36b42ae15cbad319ee44
jjang051tj <jjang051tj@gmail.com>
2024-11-14 14:45:33 +0900

    pl sql 03 cursor

--- pl_sql_04.sql
+++ pl_sql_04.sql
@@ -27,6 +27,63 @@ BEGIN
 	CLOSE c01;
 END;
 
+--여러줄일때  loop
+DECLARE
+	V_DEPT_ROW DEPT%ROWTYPE;
+	CURSOR c01 IS 
+		SELECT deptno,dname,loc
+		FROM dept;
+BEGIN
+	OPEN c01;
+	LOOP
+		FETCH c01 INTO V_DEPT_ROW;
+--		EXIT WHEN c01%NOTFOUND;
+--			DBMS_OUTPUT.PUT_LINE('DEPTNO : '|| V_DEPT_ROW.DEPTNO);
+--			DBMS_OUTPUT.PUT_LINE('DNAME  : '|| V_DEPT_ROW.DNAME);
+--			DBMS_OUTPUT.PUT_LINE('LOC    : '|| V_DEPT_ROW.LOC);
+		--  c01%NOTFOUND결과가 있으면 true 없으면 false;
+		IF c01%NOTFOUND THEN 
+			EXIT;
+		ELSE 
+			DBMS_OUTPUT.PUT_LINE('DEPTNO : '|| V_DEPT_ROW.DEPTNO);
+			DBMS_OUTPUT.PUT_LINE('DNAME  : '|| V_DEPT_ROW.DNAME);
+			DBMS_OUTPUT.PUT_LINE('LOC    : '|| V_DEPT_ROW.LOC);
+		END IF;
+	END LOOP;
+	CLOSE c01;
+END;
+--emp출력해보기....
+
+DECLARE
+	V_EMP_ROW EMP%ROWTYPE;
+	CURSOR c01 IS
+		SELECT * FROM emp;
+BEGIN 
+	OPEN c01;
+	LOOP 
+		FETCH c01 INTO 	V_EMP_ROW;
+		IF c01%NOTFOUND THEN
+			EXIT;
+		ELSE 
+			DBMS_OUTPUT.PUT_LINE('EMPNO  : '|| V_EMP_ROW.EMPNO || 
+								 ' - ENAME  : '|| V_EMP_ROW.ENAME || 
+								 '- JOB    : '|| V_EMP_ROW.JOB);
+		END IF;
+	END LOOP; 
+	CLOSE c01;
+END;
+
+-- for loop
+DECLARE
+	CURSOR c01 IS
+		SELECT * FROM emp;
+BEGIN 
+	FOR c01_rec IN c01 LOOP 
+		DBMS_OUTPUT.PUT_LINE('EMPNO  : '|| c01_rec.EMPNO || 
+							 ' - ENAME  : '|| c01_rec.ENAME || 
+							 '- JOB    : '|| c01_rec.JOB);
+	END LOOP;
+END;
 
 
 
