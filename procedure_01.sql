df30598324e45d1743fd10910c06d86954a23978
jjang051tj <jjang051tj@gmail.com>
2024-11-15 10:52:36 +0900

    pl sql procedure

--- procedure_01.sql
+++ procedure_01.sql
@@ -32,8 +32,35 @@ BEGIN
 END;
 
 BEGIN
-	pro_param_in(100,300);
+	pro_param_in(1000,2000);
+	pro_param_in(1000,2000,param03=>150);
+	pro_param_in
+	(param01=>100,param02=>300,param03=>10,param04=>500);
 END;
+
+--함수를 만들때 리턴값을 만들 수 있듯이 프로시져도 이게 가능
+CREATE OR REPLACE PROCEDURE pro_param_out(
+	in_empno IN emp.empno%TYPE,
+	out_ename OUT emp.ename%TYPE,
+	out_sal OUT emp.sal%TYPE
+)
+IS
+BEGIN
+	SELECT ename,sal INTO out_ename,out_sal FROM emp
+	WHERE empno =  in_empno;
+END pro_param_out;
+
+DECLARE 
+	v_ename emp.ename%TYPE;
+	v_sal emp.sal%TYPE;
+BEGIN
+	pro_param_out(7369,v_ename,v_sal);
+	dbms_output.put_line('ename : '|| v_ename);
+	dbms_output.put_line('sal : '|| v_sal);
+END;
+
+
+
 
 
 
